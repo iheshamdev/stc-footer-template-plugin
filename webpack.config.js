2e14e6b410e888d92a65b509c6e92a764705c721
heshamabdulzaher <heshamabdulzaher@gmail.com>
2020-01-21 22:22:18 +0200

    install `html-webpack-plugin`, handle webpack.config.js to create index.html in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -30,6 +31,10 @@ module.exports = {
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css',
