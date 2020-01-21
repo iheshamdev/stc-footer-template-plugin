e6edcfe08651f7a0c4b02ba0e5f9d203675fcc2a
heshamabdulzaher <heshamabdulzaher@gmail.com>
2020-01-21 21:53:16 +0200

    get .stc-footer element abd inject the footerTemplate on it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-console.log('dev mode...');
 import './styles/main.scss'
+import footerTemplate from './footer-template'
+console.log('dev mode...');
+
+// #1 Get all elements has `stc-footer` class
+const stcFooterElements = document.querySelectorAll('.stc-footer');
+stcFooterElements.forEach(el => el.innerHTML = footerTemplate())
